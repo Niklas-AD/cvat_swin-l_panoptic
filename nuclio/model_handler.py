de6062ca70208ac7420f55bfafa1b21539dfc34b
Niklas-AD <97914640+Niklas-AD@users.noreply.github.com>
2022-05-21 12:28:34 +0200

    Update model_handler.py

--- nuclio/model_handler.py
+++ nuclio/model_handler.py
@@ -11,12 +11,10 @@ sys.path.insert(0, "Mask2Former")
 
 #install
 from skimage.measure import approximate_polygon, find_contours
-
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import convert_PIL_to_numpy
 from detectron2.data.datasets.builtin_meta import CITYSCAPES_CATEGORIES
-
 from detectron2.projects.deeplab import add_deeplab_config
 from mask2former import add_maskformer2_config
 
@@ -39,14 +37,12 @@ class ModelHandler:
     def infer(self, image): 
         results = []
         output = self.model(image)
-        
         result_tensor = output['panoptic_seg'][0].cpu().numpy()
         segments = output['panoptic_seg'][1]
         
         for segment in segments:
             
             mask_instance = ((result_tensor == segment['id']) *255).astype(np.float32)
-
             contours = find_contours(mask_instance, 0.8)
 
             for contour in contours:
